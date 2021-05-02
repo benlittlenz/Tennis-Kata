6622ff13b9e787129bd75512ae2aa542a4e32c5f
benlittlenz <benlittle53@gmail.com>
2021-05-02 13:42:30 +1200

    added assertion to play tie-break match and logic to count tie-break points

--- dist/__tests/game.test.js
+++ dist/__tests/game.test.js
@@ -1,63 +1,54 @@
-import { Match } from '../src/Match'
-import { Player } from '../src/types'
-
-describe('Initialize tennis match', () => {
-    const playerOne: Player = "Player One";
-    const playerTwo: Player = "Player Two";
-    let match: Match;
-
-    beforeEach(() => {
-        match = new Match(playerOne, playerTwo);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Match_1 = require("../src/Match");
+describe('Initialize tennis match', function () {
+    var playerOne = "Player One";
+    var playerTwo = "Player Two";
+    var match;
+    beforeEach(function () {
+        match = new Match_1.Match(playerOne, playerTwo);
     });
-
-    const scoreShouldEqual = (expected: string) => {
+    var scoreShouldEqual = function (expected) {
         expect(match.score()).toBe(expected);
-    }
-
-    const winnerShouldEqual = (expected: string | undefined) => {
+    };
+    var winnerShouldEqual = function (expected) {
         expect(match.winner).toBe(expected);
-    }
-
-    it('Should be a Match instance', () => {
+    };
+    it('Should be a Match instance', function () {
         //console.log('HI', tennis)
-        expect(match).toBeInstanceOf(Match);
+        expect(match).toBeInstanceOf(Match_1.Match);
     });
-
-    it("should return love all", () => {
-        scoreShouldEqual("0-0, 0-0")
+    it("should return love all", function () {
+        scoreShouldEqual("0-0, 0-0");
     });
-
     /*
         The running score of each game is described in a
         manner peculiar to tennis: scores from zero to
         three points are described as 0, 15, 30, 40,
         respectively
     */
-
-    it("should return correct score", () => {
+    it("should return correct score", function () {
         match.pointWonBy(playerOne);
-        scoreShouldEqual("0-0, 15-0")
+        scoreShouldEqual("0-0, 15-0");
         match.pointWonBy(playerOne);
-        scoreShouldEqual("0-0, 30-0")
+        scoreShouldEqual("0-0, 30-0");
         match.pointWonBy(playerTwo);
-        scoreShouldEqual("0-0, 30-15")
+        scoreShouldEqual("0-0, 30-15");
         match.pointWonBy(playerOne);
-        scoreShouldEqual("0-0, 40-15")
+        scoreShouldEqual("0-0, 40-15");
     });
-
     /*
         If at least 3 points have been scored by each
         player, and the scores are equal, the score is
         "deuce".
     */
-    it("should handle deuce", () => {
-        for (let i = 0; i < 3; i++) {
+    it("should handle deuce", function () {
+        for (var i = 0; i < 3; i++) {
             match.pointWonBy(playerOne);
             match.pointWonBy(playerTwo);
         }
         scoreShouldEqual("0-0, Deuce");
     });
-
     /*
         If at least 3 points have been scored by each
         side and a player has one more point than his
@@ -77,76 +68,69 @@ describe('Initialize tennis match', () => {
     //     match.pointWonBy(playerOne);
     //     scoreShouldEqual(`0-0, Advantage ${playerOne}`);
     // });
-
-    it('Should handle player two advantage', () => {
-        for (let i = 0; i < 3; i++) {
+    it('Should handle player two advantage', function () {
+        for (var i = 0; i < 3; i++) {
             match.pointWonBy(playerOne);
             match.pointWonBy(playerTwo);
         }
         scoreShouldEqual("0-0, Deuce");
         match.pointWonBy(playerTwo);
-        scoreShouldEqual(`0-0, Advantage ${playerTwo}`);
+        scoreShouldEqual("0-0, Advantage " + playerTwo);
         match.pointWonBy(playerOne);
-        scoreShouldEqual(`0-0, Deuce`);
+        scoreShouldEqual("0-0, Deuce");
         match.pointWonBy(playerTwo);
-        scoreShouldEqual(`0-0, Advantage ${playerTwo}`);
+        scoreShouldEqual("0-0, Advantage " + playerTwo);
     });
-
-    it('should handle player one win a non deuce game', () => {
-        for (let i = 0; i < 4; i++) {
+    it('should handle player one win a non deuce game', function () {
+        for (var i = 0; i < 4; i++) {
             match.pointWonBy(playerOne);
         }
         scoreShouldEqual("1-0");
-    })
-
-    it('should handle player two win a non deuce game', () => {
-        for (let i = 0; i < 4; i++) {
+    });
+    it('should handle player two win a non deuce game', function () {
+        for (var i = 0; i < 4; i++) {
             match.pointWonBy(playerTwo);
         }
         scoreShouldEqual("0-1");
-    })
-
+    });
     /*
         A player should be able to win 6 games in a row and return 6-0
     */
-    it('should have player one as winner', () => {
-        for (let i = 0; i < 24; i++) {
+    it('should have player one as winner', function () {
+        for (var i = 0; i < 24; i++) {
             match.pointWonBy(playerOne);
         }
         scoreShouldEqual("6-0");
-    })
-
-    it('should have player two as winner', () => {
-        for (let i = 0; i < 24; i++) {
+    });
+    it('should have player two as winner', function () {
+        for (var i = 0; i < 24; i++) {
             match.pointWonBy(playerTwo);
         }
         scoreShouldEqual("0-6");
-        winnerShouldEqual("Player Two")
-    })
-
+        winnerShouldEqual("Player Two");
+    });
     /*
         If one player has won six games and the opponent five, an additional
         game is played. If the leading player wins that game, the player wins
         the set 7–5. If the trailing player wins the game, a tie-break is played.
     */
-    it('should assert p1 wins 7-5', () => {
-        for (let i = 0; i < 24; i++) {
+    it('should assert p1 wins 7-5', function () {
+        for (var i = 0; i < 24; i++) {
             match.pointWonBy(playerOne);
         }
-        for (let j = 0; j < 20; j++) {
+        for (var j = 0; j < 20; j++) {
             match.pointWonBy(playerTwo);
         }
         scoreShouldEqual("6-5");
         //for (let i = 0; i < 2; i++) {
-        for (let i = 0; i < 4; i++) {
+        for (var i = 0; i < 4; i++) {
             match.pointWonBy(playerOne);
         }
         //match.pointWonBy(playerTwo);
         //}
         scoreShouldEqual("7-5");
-        winnerShouldEqual("Player One")
-    })
-
+        winnerShouldEqual("Player One");
+    });
     /*
         A tie-break, played under a separate set of rules, allows one player to win
         one more game and thus the set, to give a final set score of 7–6. A tie-break is
@@ -155,26 +139,31 @@ describe('Initialize tennis match', () => {
         like regular games, the score for a tie breaker goes up incrementally from 0 by 1.
         i.e a player's score will go from 0 to 1 to 2 to 3 …etc.
     */
-
-    it("should show tie breaks with a final set score of 7-6", () => {
-        for (let i = 0; i < 24; i++) {
+    it("should show correct tie break points when one player win at 7-5", function () {
+        for (var i = 0; i < 24; i++) {
             match.pointWonBy(playerOne);
         }
-        for (let j = 0; j < 24; j++) {
+        for (var j = 0; j < 20; j++) {
             match.pointWonBy(playerTwo);
         }
-        scoreShouldEqual("6-6");
-
-        for (let i = 0; i < 5; i = i + 1) {
-          match.pointWonBy(playerOne);
-          match.pointWonBy(playerTwo);
-        }
-
-        expect(match.score()).toEqual("6-6, 5-5");
-        match.pointWonBy(playerOne);
-        expect(match.score()).toEqual("6-6, 6-5");
-        match.pointWonBy(playerOne);
-        expect(match.score()).toEqual("7-6");
-
+        scoreShouldEqual("6-5");
+        // for (let i = 0; i < 6; i = i + 1) {
+        //     for (let j = 0; j < 4; j = j++) {
+        //         match.pointWonBy(playerOne);
+        //     }
+        //     for (let j = 0; j < 4; j = j++) {
+        //         match.pointWonBy(playerTwo);
+        //     }
+        // }
+        // scoreShouldEqual("7-5");
+        // for (let i = 0; i < 5; i = i + 1) {
+        //   match.pointWonBy(playerOne);
+        //   match.pointWonBy(playerTwo);
+        // }
+        // expect(match.score()).toEqual("6-6, 5-4");
+        // match.pointWonBy(playerOne);
+        // expect(match.score()).toEqual("6-6, 6-5");
+        // match.pointWonBy(playerOne);
+        // expect(match.score()).toEqual("7-6");
     });
-})
+});
--- dist/src/Match.js
+++ dist/src/Match.js
@@ -1,161 +1,128 @@
-import { Player, Score, Game, Deuce, Tie } from './types'
-
-const pointLookup: { [key: number]: string } = {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Match = void 0;
+var pointLookup = {
     0: '0',
     1: '15',
     2: '30',
     3: '40',
     4: 'Advantage'
 };
-
-export class Match {
-    public winner?: Player;
-
-    private players: [Player, Player];
-    private points: [Score, Score];
-    private games: [Game, Game];
-    private deuces: [Deuce, Deuce];
-    private ties: [Tie, Tie];
-
-    constructor(playerOneName: Player, playerTwoName: Player) {
+var Match = /** @class */ (function () {
+    function Match(playerOneName, playerTwoName) {
         this.players = [playerOneName, playerTwoName];
         this.points = [0, 0];
         this.games = [0, 0];
         this.deuces = [0, 0];
-        this.ties = [0, 0];
     }
-
-    public pointWonBy(player: Player): void {
-        const index = this.players.findIndex((p: Player) => p === player)
+    Match.prototype.pointWonBy = function (player) {
+        var index = this.players.findIndex(function (p) { return p === player; });
         this.points[index] = this.points[index] + 1;
-
         //console.log(this.points)
-        if(this.isTieBreak()) {
-            this.ties[index] = this.ties[index] + 1;
-        }
         if (this.isDeuce()) {
             this.deuces[index] = this.deuces[index] + 1;
+            // if (Math.abs(this.deuces[0] - this.deuces[1]) >= 2) {
+            //     // player doesn't win the game when tie break point is less then 7
+            //     if (this.isTieBreak() && Math.max(this.deuces[0], this.deuces[1]) < 7) {
+            //       return;
+            //     }
+            //     this.games[index] = this.games[index] + 1;
+            //     // reset deuce and point index
+            //     this.deuces = [0, 0];
+            //     this.points = [0, 0];
+            //   }
         }
-
         /*
             Increment game count by players game count
         */
         if (this.playerOneWonGame()) {
             this.games[0] = this.games[0] + 1;
         }
-
         if (this.playerTwoWonGame()) {
             this.games[1] = this.games[1] + 1;
         }
-
         if (this.playerOneWonMatch()) {
             this.winner = this.players[index];
         }
         if (this.playerTwoWonMatch()) {
             this.winner = this.players[index];
         }
-    }
-
-    public score(): string {
-        const games = `${this.games[0]}-${this.games[1]}`
-        const points = this.getScore();
+    };
+    Match.prototype.score = function () {
+        var games = this.games[0] + "-" + this.games[1];
+        var points = this.getScore();
         //console.log('POINTS: ', points)
         if (points === "") {
             return games;
         }
-
-        return `${games}, ${points}`;
-    }
-
-    public isDeuce(): boolean {
+        return games + ", " + points;
+    };
+    Match.prototype.isDeuce = function () {
         return this.pointsAreEqual() && (this.points[0] >= 3 && this.points[1] >= 3) ? true : false;
-    }
-
-    public isTieBreak(): boolean {
-        if (this.games.every((game: number) => game === 6)) {
-            return true;
-        }
-        return false;
-    }
-
-    public isAdvantagePlayerOne(): boolean {
+    };
+    // public isTieBreak(): boolean {
+    //     if (this.games.every((game: number) => game === 6)) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+    Match.prototype.isAdvantagePlayerOne = function () {
         return this.points[0] >= 4 && this.points[0] - this.points[1] === 1 ? true : false;
-    }
-
-    public isAdvantagePlayerTwo(): boolean {
+    };
+    Match.prototype.isAdvantagePlayerTwo = function () {
         return this.points[1] >= 4 && this.points[1] - this.points[0] === 1 ? true : false;
-    }
-
+    };
     /*
         A player wins a game by winning at least
     */
-    public playerOneWonGame(): boolean {
+    Match.prototype.playerOneWonGame = function () {
         if (this.points[0] >= 4 && (this.points[0] - this.points[1] >= 2)) {
             this.points = [0, 0];
             return true;
         }
         return false;
-    }
-
-    public playerTwoWonGame(): boolean {
+    };
+    Match.prototype.playerTwoWonGame = function () {
         if (this.points[1] >= 4 && (this.points[1] - this.points[0] >= 2)) {
             this.points = [0, 0];
             return true;
         }
         return false;
-    }
-
-    public playerOneWonMatch(): boolean {
+    };
+    Match.prototype.playerOneWonMatch = function () {
         if (this.games[0] >= 6 && (this.games[0] - this.games[1] >= 2)) {
             return true;
         }
         return false;
-    }
-
-    public playerTwoWonMatch(): boolean {
+    };
+    Match.prototype.playerTwoWonMatch = function () {
         if (this.games[1] >= 6 && (this.games[1] - this.games[0] >= 2)) {
             return true;
         }
         return false;
-    }
-
-    public assertWinner(): Player {
-
-        return this.players[0];
-    }
-
-    public getScore(): string {
-        if (this.isTieBreak()) {
-            return this.getTieBreakScore();
-        }
+    };
+    Match.prototype.getScore = function () {
+        // if (this.isTieBreak()) {
+        //     return this.getTieBreakScore();
+        // }
         if (this.isDeuce()) {
             return 'Deuce';
         }
-
         if (this.isAdvantagePlayerOne()) {
-            return `Advantage ${this.players[0]}`
+            return "Advantage " + this.players[0];
         }
-
         if (this.isAdvantagePlayerTwo()) {
-            return `Advantage ${this.players[1]}`
+            return "Advantage " + this.players[1];
         }
         // console.log("YO: ", this.points[0])
         if ((this.points[0] === 0 && this.points[1] === 0) && this.games[0] > 0 || this.games[1] > 0) {
             return "";
         }
-
-        return `${pointLookup[this.points[0]]}-${pointLookup[this.points[1]]}`;
-    }
-
-    public pointsAreEqual(): boolean {
+        return pointLookup[this.points[0]] + "-" + pointLookup[this.points[1]];
+    };
+    Match.prototype.pointsAreEqual = function () {
         return this.points[0] === this.points[1] ? true : false;
-    }
-
-    public getTieBreakScore(): string {
-        if (this.ties.every((d: number) => d === 0)) {
-            return "";
-        }
-
-        return `${this.ties[0]}-${this.ties[1]}`
-    }
-}
+    };
+    return Match;
+}());
+exports.Match = Match;
